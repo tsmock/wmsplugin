014f889183746981cffd3c275b3010fa0b3b9951
Gabriel Ebner <ge@gabrielebner.at>
2008-02-04 14:50:57 +0000

    wmsplugin: Fix downloading after canceling.

---
+++ src/wmsplugin/WMSInfo.java
@@ -0,0 +1,25 @@
+package wmsplugin;
+
+import org.openstreetmap.josm.Main;
+
+/**
+ * Class that stores info about a WMS server.
+ * 
+ * @author Frederik Ramm <frederik@remote.org>
+ */
+public class WMSInfo {
+    
+    String name;
+    String url;
+    int prefid;
+    
+    public WMSInfo(String name, String url, int prefid) {
+        this.name=name; this.url=url; this.prefid=prefid;
+    }
+    
+    public void save() {
+        Main.pref.put("wmsplugin.url." + prefid + ".name", name);
+        Main.pref.put("wmsplugin.url." + prefid + ".url", url);
+    }
+    
+}
